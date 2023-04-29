4ba43ad24554d8ea77b8335596221a5e87dd4346
Saliou Akande <lawalsaliouakande@gmail.com>
2023-04-29 10:08:57 +0100

    new commit

--- src/views/product/ProductDetail.vue
+++ src/views/product/ProductDetail.vue
@@ -34,7 +34,6 @@
               <p class="font-medium text-2xl md:text-4xl">
                 {{ product.name }}
               </p>
-              <p>Number: {{ cartLength }}</p>
               <p class="font-bold text-xl">${{ product.price }}</p>
             </div>
             <div class="space-y-1">
@@ -60,7 +59,26 @@
                 @click="addToCart"
                 class="w-full font-windsor-pro-bold bg-ShamrockGreen p-3 text-white"
               >
-                Add to Cart
+                <span v-if="isLoading">
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    class="inline w-4 h-4 mr-3 text-white animate-spin"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="#E5E7EB"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </span>
+                <span v-else>Add to Cart</span>
               </button>
               <!-- <button class="border border-black p-4">
                 <img src="../../assets/icons/fi_small_heart.svg" alt="" />
@@ -78,8 +96,8 @@
 </template>
 
 <script>
-// import { toast } from "vue3-toastify";
-// import "vue3-toastify/dist/index.css";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 import { mapMutations } from "vuex";
 import Navbar from "@/components/Navbar.vue";
 
@@ -99,6 +117,7 @@ export default {
       id: null,
       quantity: 1,
       numberOfProductInCart: [],
+      isLoading: false,
     };
   },
 
@@ -119,9 +138,10 @@ export default {
       });
       this.products = this.$store.state.data.productList;
     },
-
     async addToCart() {
-      
+      // Set isLoading to true to display loader
+      this.isLoading = true;
+
       let cartInput;
       let checkCartId = window.localStorage.getItem("cartId");
       if (checkCartId) {
@@ -141,24 +161,21 @@ export default {
         endpoint: "addCart",
         data: { input: cartInput },
       });
-      if(response) {
+
+      // Set isLoading to false to remove loader
+      this.isLoading = false;
+
+      if (response) {
         const viewaddCartId = localStorage.getItem("cartId");
-      await this.$store.dispatch("query", {
-        endpoint: "viewaddCart",
-        storeKey: "cartList",
-        payload: { viewaddCartId: viewaddCartId },
-      });
+        await this.$store.dispatch("query", {
+          endpoint: "viewaddCart",
+          storeKey: "cartList",
+          payload: { viewaddCartId: viewaddCartId },
+        });
       }
       window.localStorage.setItem("cartId", response._id);
-      // this.$store.state.cartId =response._id
       console.log("res", response);
       this.numberOfProductInCart = response.productId;
-      // this.$store.commit(
-      //   "updateNumberOfProductsInCart",
-      //   response.productId.length
-      // );
-      console.log("number of items", response.productId.length);
-      
 
       if (this.quantity || this.quantity < 1) {
         this.quantity = 1;

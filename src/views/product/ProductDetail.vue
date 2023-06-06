75281640debeb8c6cd31dd9beeeb9a90b9382376
Saliou Akande <lawalsaliouakande@gmail.com>
2023-06-06 13:46:41 +0100

    recent commit

--- src/views/product/ProductDetail.vue
+++ src/views/product/ProductDetail.vue
@@ -62,10 +62,10 @@
         </div>
         <div v-else>
           <div
-            class="xl:px-40 flex flex-col space-y-10 lg:space-y-0 lg:flex-row lg:space-x-16"
+            class="xl:px-40 flex flex-col space-y-10 lg:space-y-0 lg:flex-row lg:space-x-16 lg:h-[550px]"
           >
             <div
-              class="lg:w-7/12 flex flex-col-reverse lg:flex-row items-start lg:space-x-10"
+              class="lg:w-7/12 flex flex-col-reverse lg:flex-row items-start lg:space-x-10 px-10 md:px-0"
             >
               <div
                 class="hidden lg:block md:w-1/12 w-full md:flex flex-row space-x-2.5 lg:space-x-0 lg:flex-col lg:space-y-2.5 mt-10 lg:mt-0"
@@ -93,67 +93,10 @@
                 />
               </div>
             </div>
-            <!-- <div
-              class="lg:w-7/12 flex flex-col-reverse lg:flex-row items-start lg:space-x-10"
-            >
+            <div class="w-full lg:w-5/12">
               <div
-                class="hidden lg:block md:w-1/12 w-full md:flex flex-row space-x-2.5 lg:space-x-0 lg:flex-col lg:space-y-2.5 mt-10 lg:mt-0"
+                class="w-full space-y-8 flex flex-col justify-between lg:h-[550px]"
               >
-                <div class="w-14 h-14 bg-BalticSea">
-                  <img
-                    class="h-full w-full object-cover"
-                    :src="product.images[0]"
-                    :alt="product.name"
-                  />
-                </div>
-                <div v-show="product.images[1]" class="w-14 h-14 bg-BalticSea">
-                  <img
-                    class="h-full w-full object-cover"
-                    :src="product.images[1]"
-                    :alt="product.name"
-                  />
-                </div>
-                <div v-show="product.images[2]" class="w-14 h-14 bg-BalticSea">
-                  <img
-                    class="h-full w-full object-cover"
-                    :src="product.images[2]"
-                    :alt="product.name"
-                  />
-                </div>
-                <div v-show="product.images[3]" class="w-14 h-14 bg-BalticSea">
-                  <img
-                    class="h-full w-full object-cover"
-                    :src="product.images[3]"
-                    :alt="product.name"
-                  />
-                </div>
-                <div v-show="product.images[4]" class="w-14 h-14 bg-BalticSea">
-                  <img
-                    class="h-full w-full object-cover"
-                    :src="product.images[4]"
-                    :alt="product.name"
-                  />
-                </div>
-                <div v-show="product.images[5]" class="w-14 h-14 bg-BalticSea">
-                  <img
-                    class="h-full w-full object-cover"
-                    :src="product.images[5]"
-                    :alt="product.name"
-                  />
-                </div>
-              </div>
-              <div
-                class="w-full lg:w-11/12 bg-LightGrey h-[350px] md:h-[550px] border-[3px] border-DarkJungleGreen"
-              >
-                <img
-                  class="h-full w-full object-cover"
-                  :src="product.images[0]"
-                  :alt="product.name"
-                />
-              </div>
-            </div> -->
-            <div class="w-full lg:w-5/12 flex flex-row">
-              <div class="w-full h-full space-y-8">
                 <div class="space-y-2 font-windsor-pro-bold text-BalticSea">
                   <p class="font-medium text-2xl md:text-5xl">
                     {{ product.name }}
@@ -180,9 +123,9 @@
                     ></VueMultiselect>
                   </div> -->
                   <div v-if="sizes.length > 0">
-                    <h4>Available Sizes:</h4>
+                    <h4>Sizes:</h4>
                     <select
-                      class="w-1/2 border px-3 py-2 focus:outline-none focus:border-BrownBramble focus:ring-0"
+                      class="w-full border px-3 py-2 focus:outline-none focus:border-BrownBramble focus:ring-0"
                       v-model="selectedSize"
                     >
                       <option v-for="size in sizes" :key="size" :value="size">
@@ -192,9 +135,9 @@
                   </div>
 
                   <div v-if="colors.length > 0">
-                    <h4>Available Colors:</h4>
+                    <h4>Colors:</h4>
                     <select
-                      class="w-1/2 border px-3 py-2 focus:outline-none focus:border-BrownBramble focus:ring-0"
+                      class="w-full border px-3 py-2 focus:outline-none focus:border-BrownBramble focus:ring-0"
                       v-model="selectedColor"
                     >
                       <option
@@ -211,41 +154,42 @@
                     <input
                       type="number"
                       v-model="quantity"
-                      class="w-1/2 border px-3 py-2 focus:outline-none focus:border-BrownBramble focus:ring-0"
+                      class="w-full border px-3 py-2 focus:outline-none focus:border-BrownBramble focus:ring-0"
                     />
                   </div>
                 </div>
-
-                <div class="max-w-[22rem]">
-                  <p class="text-base font-medium mb-2">Description</p>
-                  {{ product.description }}
-                </div>
-                <div class="flex flex-row items-center space-x-5">
-                  <button
-                    @click="addToCart"
-                    class="w-full font-windsor-pro-bold bg-BalticSea p-3 text-white"
-                  >
-                    <span v-if="isLoading">
-                      <svg
-                        aria-hidden="true"
-                        role="status"
-                        class="inline w-4 h-4 mr-3 text-white animate-spin"
-                        viewBox="0 0 100 101"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                          fill="#E5E7EB"
-                        />
-                        <path
-                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </span>
-                    <span v-else>Add to Cart</span>
-                  </button>
+                <div class="space-y-4">
+                  <div class="max-w-[22rem]">
+                    <p class="text-base font-medium mb-2">Description</p>
+                    {{ product.description }}
+                  </div>
+                  <div class="flex flex-row items-center space-x-5">
+                    <button
+                      @click="addToCart"
+                      class="w-full font-windsor-pro-bold bg-BalticSea p-3 text-white"
+                    >
+                      <span v-if="isLoading">
+                        <svg
+                          aria-hidden="true"
+                          role="status"
+                          class="inline w-4 h-4 mr-3 text-white animate-spin"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="#E5E7EB"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                      </span>
+                      <span v-else>Add to Cart</span>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>

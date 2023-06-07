456b6011879881ce178000733d39f19c7864bddf
Saliou Akande <lawalsaliouakande@gmail.com>
2023-06-07 20:16:07 +0100

    latest commit

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -24,6 +24,7 @@
             </div>
           </div>
           <router-link
+            @click="clearLocalStorage"
             to="/"
             class="bg-ShamrockGreen text-white rounded-lg p-4"
           >
@@ -416,12 +417,12 @@ export default {
       setTimeout(() => {
         this.isOrderPlaced = true;
         this.isPlaceOrderLoading = false;
-        window.localStorage.removeItem("cartId");
-        window.localStorage.removeItem("detailId");
-        window.localStorage.removeItem("itemIds"); // itemIds
-        window.localStorage.removeItem("updateProductId");
-        window.localStorage.removeItem("cartItemIds");
-        window.localStorage.removeItem("customerId");
+        // window.localStorage.removeItem("cartId");
+        // window.localStorage.removeItem("detailId");
+        // window.localStorage.removeItem("itemIds");
+        // window.localStorage.removeItem("updateProductId");
+        // window.localStorage.removeItem("cartItemIds");
+        // window.localStorage.removeItem("customerId");
       }, 2000); // Hide the toast after 5 seconds
     },
 
@@ -431,6 +432,15 @@ export default {
     },
 
     // Other methods and lifecycle hooks...
+
+    clearLocalStorage() {
+      localStorage.removeItem("itemIds");
+      localStorage.removeItem("cartId");
+      localStorage.removeItem("detailId");
+      localStorage.removeItem("updateProductId");
+      localStorage.removeItem("cartItemIds");
+      localStorage.removeItem("customerId");
+    },
   },
   async created() {
     this.args.cartId = localStorage.getItem("cartId");
